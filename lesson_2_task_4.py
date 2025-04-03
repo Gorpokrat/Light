a13c866c4b684f7682e3484e5937f0f93be8e0e1
Gorpokrat <11kateblack@tutamail.com>
2025-04-03 14:19:57 +0300

    Create lesson_2_task_4.py
    optional

---
+++ lesson_2_task_4.py
@@ -0,0 +1,15 @@
+def fizz_buzz(n):
+    """Печатает числа от 1 до n с заменой на Fizz, Buzz и FizzBuzz."""
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+#Пример
+n = 15 
+fizz_buzz(n)
